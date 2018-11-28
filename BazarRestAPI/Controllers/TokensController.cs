d89b2f4e653d6c7d0574f707619aafe96a47b5a9
Husso1997 <hussain1961@hotmail.com>
2018-11-28 10:06:21 +0100

    Added controller - needs indhold

---
+++ BazarRestAPI/Controllers/TokensController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BazarRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokensController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public void Login([FromBody] string value)
+        {
+            // Mangler indhold
+        }
+    }
+}
