57508f458ee8adc6db46875711d30336fd240b20
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-11-28 09:35:18 +0100

    Added interfaces

---
+++ Core/Application/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Application
+{
+    public interface IUserService
+    {
+        User Create(User user);
+        IEnumerable<User> GetAll();
+        User GetByID(int id);
+        User Update(User user);
+        User Delete(int id);
+        User Login(User login);
+    }
+}
---
+++ Core/Domain/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain
+{
+    public interface IRepository<T>
+    {
+        T Add(T entity);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        T Update(T entity);
+        T Delete(int id);
+        int Count();
+    }
+}
