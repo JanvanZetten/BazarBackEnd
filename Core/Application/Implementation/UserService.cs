e9928536d8d45bd4ee41642fd26f569bc383fc1f
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-11-28 12:23:02 +0100

    Refactored so that some recommendations are not shown in Error List

--- Core/Application/Implementation/UserService.cs
+++ Core/Application/Implementation/UserService.cs
@@ -19,9 +19,9 @@ namespace Core.Application.Implementation
         {
             if (user == null)
                 throw new ArgumentNullException("The user is null.");
-            if (InputCheck.ValidLength("username", user.Username, 3, 40));
-            if (InputCheck.ValidLength("password", password, 8, 40));
-            if (InputCheck.ValidPassword(password));
+            if (InputCheck.ValidLength("username", user.Username, 3, 40)
+                && InputCheck.ValidLength("password", password, 8, 40)
+                && InputCheck.ValidPassword(password)){}
             if (!_userRepository.UniqueUsername(user.Username))
                 throw new ArgumentException("Username is already taken.");
 
