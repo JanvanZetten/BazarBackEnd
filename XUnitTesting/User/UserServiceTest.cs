c2dee5ffc3957548638beca8d586a51d692a9851
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-11-28 09:40:28 +0100

    Added test project

---
+++ XUnitTesting/User/UserServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTesting.User
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
