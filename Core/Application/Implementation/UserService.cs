299d74db27f3cdd49aba24e5d91306fb52350d72
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-11-28 09:41:28 +0100

    Saved everything just to make sure

--- Core/Application/Implementation/UserService.cs
+++ Core/Application/Implementation/UserService.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Core.Entity;
 
 namespace Core.Application.Implementation
 {
-    class UserService
+    public class UserService : IUserService
     {
+        public User Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- infrastructure/UserRepository.cs
+++ infrastructure/UserRepository.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Core.Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace infrastructure
 {
-    class UserRepository
+    public class UserRepository : IRepository<T>
     {
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
