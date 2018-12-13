ce27ec3e9dc0c1b9d1502fa274c641bd69e80aaf
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-12-13 11:10:48 +0100

    Made more create tests

--- XUnitTesting/LogTest/CreateLogTest.cs
+++ XUnitTesting/LogTest/CreateLogTest.cs
@@ -1,11 +1,13 @@
 ﻿using Core.Application;
 using Core.Application.Implementation;
+using Core.Application.Implementation.CustomExceptions;
 using Core.Domain;
 using Core.Entity;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace XUnitTesting.LogTest
 {
@@ -25,14 +27,89 @@ namespace XUnitTesting.LogTest
                 return dictionary[l.Id];
                 
             });
+
+
+            mockUserRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns<int>((i) =>
+            {
+                if(i == 1)
+                {
+                    return new User() { Id = 1, Username = "Hussein" };
+                }
+                throw new UserNotFoundException();
+            });
+
             _service = new LogService(mockLogRepository.Object, mockUserRepository.Object);
         }
 
+        [Fact]
         public void ValidLog()
         {
             Log log = new Log() { Id = 50, Date = DateTime.Today, Message = "Asbjørn elsker bjørne" };
 
-            
+            var result = _service.Create(log);
+
+            Assert.True(result.Id != log.Id && result.Id == 1);
+            Assert.Equal(log.Message, result.Message);
+            Assert.Equal(log.Date, result.Date);
+            Assert.Null(result.User);
+        }
+
+        [Fact]
+        public void InvalidDateLog()
+        {
+            Log log = new Log() { Id = 50, Message = "Asbjørn elsker bjørne" };
+
+            Assert.Throws<InputNotValidException>(() => 
+            {
+                _service.Create(log);
+            });
+        }
+
+        [Fact]
+        public void InvalidMessageLog()
+        {
+            Log log = new Log() { Id = 50, Date = DateTime.Today };
+
+            Assert.Throws<InputNotValidException>(() =>
+            {
+                _service.Create(log);
+            });
+        }
+
+        [Fact]
+        public void InvalidUserLog()
+        {
+            Log log = new Log() { Id = 50, Date = DateTime.Today, Message = "Asbjørn elsker bjørne", User = new User() { Id=50 } };
+
+            Assert.Throws<UserNotFoundException>(() =>
+            {
+                _service.Create(log);
+            });
+        }
+
+        [Fact]
+        public void NullLog()
+        {
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                _service.Create(null);
+            });
+        }
+
+        [Fact]
+        public void LogWithIdSet()
+        {
+            Log log = new Log() { Id = 50, Date = DateTime.Today, Message = "Asbjørn elsker bjørne" };
+
+            mockLogRepository.Setup(x => x.Create(It.IsAny<Log>())).Returns<Log>((l) =>
+            {
+                dictionary.Add(l.Id, l);
+                return dictionary[l.Id];
+            });
+
+            var result = _service.Create(log);
+
+            Assert.True(result.Id == 0);
         }
     }
 }
